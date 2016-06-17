28906431dabf6eaa543b378d06ae734e3087d2f6
Eric Liu <ericliu@eric-mbpi5.attlocal.net>
2016-06-17 13:07:12 -0700

    Added 1st package page with basic carousel

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'AboutCtrl', 'NerdService', 'HowItWorksCtrl', 'GeekService', 'ContactCtrl', 'BrowseCtrl', 'SignUpCtrl', 'ui.bootstrap', 'ProfileCtrl', 'rzModule']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'AboutCtrl', 'NerdService', 'HowItWorksCtrl', 'GeekService', 'ContactCtrl', 'BrowseCtrl', 'SignUpCtrl', 'ui.bootstrap', 'ProfileCtrl', 'rzModule', 'PackageCtrl']);
 
 
